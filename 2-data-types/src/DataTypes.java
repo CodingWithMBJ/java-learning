4fe625ed8b60e5420be2264d15742dbf81b509cf
CodingWithMBJ <codingwithmbj@gmail.com>
2025-10-02 22:37:34 -0500

    first commit

---
+++ 2-data-types/src/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Two Types of Data Types Categories: Primitive Types & Reference Types
+
+        // 1. Primitive Types
+
+        String name;
+        int quantity;
+        double price;
+        boolean status;
+
+
+    }
+}
